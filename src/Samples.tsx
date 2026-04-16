44a6385143a5b76d2b8cd4a7bf11238fe6c217c7
Alexander Ilseman <alexander.ilseman@gmail.com>
2026-04-16 15:02:08 -0500

    Lines drawn on chart with click - need to refactor

--- src/Samples.tsx
+++ src/Samples.tsx
@@ -1,6 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState, useRef, type MouseEvent } from "react";
 import "./Samples.css";
 import Control from "./Control";
+import decode from "audio-decode";
+import type { ChartData, ChartOptions } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Legend,
+  Plugin,
+} from "chart.js";
+import { Chart, getDatasetAtEvent } from "react-chartjs-2";
+
+import { getRelativePosition } from "chart.js/helpers";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Legend,
+);
 
 interface Sample {
   id: number;
@@ -9,15 +33,23 @@ interface Sample {
   source: string;
   created: string;
 }
+
+interface LineProps {
+  options: ChartOptions<"line">;
+  data: ChartData<"line">;
+}
+
+const audioContext = new window.AudioContext();
 export default function SampleList({ samples }: { samples: Sample[] }) {
   const [audioBuffer, setAudioBuffer] = useState<AudioBuffer | null>(null);
   const [currentSample, setCurrentSample] = useState<Sample | null>(null);
+  const [xaxis, setXaxis] = useState<number[]>([]);
+  const [dsData, setDsData] = useState<number[]>([]);
   const url = currentSample?.path
     ? `http://localhost:3000/${currentSample.path}`
     : null;
 
   const play = () => {
-    const audioContext = new window.AudioContext();
     const source = audioContext.createBufferSource();
     source.buffer = audioBuffer;
     source.connect(audioContext.destination);
@@ -31,8 +63,38 @@ export default function SampleList({ samples }: { samples: Sample[] }) {
         try {
           const response = await fetch(url);
           const arrayBuffer = await response.arrayBuffer();
-          const audioContext = new window.AudioContext();
+          let { channelData, sampleRate } = await decode(arrayBuffer);
+          let dsData = [] as number[];
+          let acc = 0;
+
+          const downsampleRate = 100;
+          const samplesPerSecond = sampleRate / downsampleRate;
+          channelData[0].forEach((val, idx) => {
+            acc += Math.abs(val);
+            if ((idx + 1) % downsampleRate === 0) {
+              dsData.push(acc / downsampleRate);
+              acc = 0;
+            }
+          });
+          console.log(`dsData length: ${dsData.length}`);
+          const xaxis = dsData.map((_value, index) => {
+            return index / samplesPerSecond;
+          });
+          setDsData(dsData);
+          setXaxis(xaxis);
+          console.log(xaxis);
+          console.log(sampleRate);
+          console.log(samplesPerSecond);
+
+          // const audioContext = new window.AudioContext();
+          // const analyzer = audioContext.createAnalyser();
+          // analyzer.fftSize = 2048;
+          // const bufferLength = analyzer.frequencyBinCount;
+          // const dataArray = new Uint8Array(bufferLength);
           const decodedBuffer = await audioContext.decodeAudioData(arrayBuffer);
+          const source = audioContext.createBufferSource();
+          source.buffer = audioBuffer;
+          // source.connect(analyzer);
           setAudioBuffer(decodedBuffer);
         } catch (error) {
           console.error("Error loading the audio: ", error);
@@ -42,6 +104,42 @@ export default function SampleList({ samples }: { samples: Sample[] }) {
 
     loadAudio();
   }, [url]);
+
+  const chartRef = useRef<ChartJS>(null);
+  const overlays = useRef<{ x: number; y: number }[]>([]);
+
+  const onClick = (event: MouseEvent<HTMLCanvasElement>) => {
+    const chart = chartRef.current;
+
+    if (!chart) return;
+
+    console.log(getRelativePosition(event.nativeEvent, chart));
+
+    const position = getRelativePosition(event.nativeEvent, chart);
+    overlays.current.push(position);
+    chart.draw();
+  };
+
+  const playStartDrawPI: Plugin<"line"> = useMemo(
+    () => ({
+      id: "playStart",
+      afterDraw: (chart: ChartJS) => {
+        const { top, bottom } = chart.chartArea;
+        const ctx = chart.ctx;
+
+        overlays.current.forEach(({ x, y }) => {
+          ctx.save();
+          ctx.lineWidth = 1;
+          ctx.beginPath();
+          ctx.moveTo(x, top);
+          ctx.lineTo(x, bottom);
+          ctx.stroke();
+          // ctx.restore();
+        });
+      },
+    }),
+    [],
+  );
 
   return (
     <div className="samplesComponent">
@@ -61,6 +159,35 @@ export default function SampleList({ samples }: { samples: Sample[] }) {
               {sample.name}
             </div>
           ))}
+        </div>
+        <div>
+          <Chart
+            plugins={[playStartDrawPI]}
+            type="line"
+            ref={chartRef}
+            onClick={onClick}
+            options={{
+              scales: {
+                x: {
+                  ticks: {
+                    callback: function (val, index) {
+                      return (
+                        Math.round(
+                          Number.parseFloat(
+                            this.getLabelForValue(val as number),
+                          ) * 100,
+                        ) / 100
+                      );
+                    },
+                  },
+                },
+              },
+            }}
+            data={{
+              labels: xaxis,
+              datasets: [{ label: "sample", data: dsData, pointStyle: false }],
+            }}
+          />
         </div>
         <div className="player">
           Audio loaded: {audioBuffer ? <Control handleClick={play} /> : "No"}
